515afaf8b81b23d892770ae042df7b992a2ca286
sajanmje27 <sajanmje82@gmail.com>
2021-10-14 21:58:17 -0400

    draft

---
+++ src/test/java/cana/codelessautomation/scheduler/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package cana.codelessautomation.scheduler;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
